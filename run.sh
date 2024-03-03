0f9e9e969cd6d56505ccba86eb839087fc4adc17
Simon Hornby <liquidwicked64@gmail.com>
2024-03-03 19:23:29 +0200

    refactor: features are now read from the server in the run script and form part of the run id

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 
+get_features_hash() {
+    local features_url=$1
+    curl -s "$features_url" | sha256sum | awk '{ print substr($1, 1, 6) }'
+}
+
 get_file_hash() {
     local file_path="$1"
 
@@ -15,15 +20,25 @@ get_file_hash() {
 run_container() {
     local language_tag=$1
     local test_file=$2
-    local features_file=$3
+    local seidr_url="http://localhost:4242/api/client/features"
+
+    local features_hash
+    if ! features_hash=$(get_features_hash "$seidr_url"); then
+        echo "Failed to get features from $seidr_url. Is Seidr running?."
+        return 1
+    fi
 
     local file_hash
     if ! file_hash=$(get_file_hash "$test_file"); then
         echo "Failed to get file hash for $test_file. Exiting."
         return 1
     fi
+    local destination_path="testruns/$features_hash/$file_hash"
 
-    mkdir -p testruns/$file_hash
+    mkdir -p "$destination_path"
+
+    curl -s "$seidr_url" -o "testruns/$features_hash/client_features.json"
+    cp "$test_file" "$destination_path/test_file.json"
 
     IFS=':' read -r language tag <<< "$language_tag"
     tag=${tag:-main} # Default to 'main' if no tag is provided, is this a good idea?
@@ -44,7 +59,7 @@ run_container() {
         if [ "$debug_mode" = "true" ]; then
             cat "$test_file" | docker run -i -e SEIDR_DEBUG="$debug_mode" -e UNLEASH_API_URL="http://seidr-core:4242/api/" --name $container_name --network seidr-network $image_tag
         else
-            cat "$test_file" | docker run -i -e SEIDR_DEBUG="$debug_mode" -e UNLEASH_API_URL="http://seidr-core:4242/api/" --name $container_name --network seidr-network $image_tag > testruns/${file_hash}/${language}-${tag}-output.txt
+            cat "$test_file" | docker run -i -e SEIDR_DEBUG="$debug_mode" -e UNLEASH_API_URL="http://seidr-core:4242/api/" --name $container_name --network seidr-network $image_tag > ${destination_path}/${language}-${tag}-output.txt
         fi
     else
         echo "Dockerfile for $language not found."
@@ -52,10 +67,10 @@ run_container() {
     fi
 }
 
-if [ $# -lt 3 ]; then
+if [ $# -lt 2 ]; then
     echo "Insufficient arguments provided."
-    echo "Usage: $0 <language[:tag]> <test_file> <features_file>"
+    echo "Usage: $0 <language[:tag]> <test_file>"
     exit 1
 fi
 
-run_container $1 $2 $3
+run_container $1 $2
